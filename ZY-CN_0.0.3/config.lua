1f669787fd94656096233176d40678173671ebee
朱银 <114227657+zhuyin520@users.noreply.github.com>
2023-09-11 02:39:13 +0800

    Add files via upload

---
+++ ZY-CN_0.0.3/config.lua
@@ -0,0 +1,23 @@
+my_light_custom = true               -- 如果为 true，个人灯光将根据以下参数进行更改。 如果为 false，则什么都没有改变。
+my_light_minimum_darkness = 0.01     -- 个人灯打开时的黑暗级别（最初为 0.3）
+my_light_intensity = 2               -- 整体个人光线强度（原为 0.4）
+my_light_size = 100000               -- 整体个人灯光的大小/半径（最初为 25）
+my_light_cone_intensity = 0          -- 个人光锥的强度（原为 0.6）
+                                     -- 如果相反，你想要更多的黑暗，请尝试：                      
+
+my_light_locomotive_still = true     -- 机车灯不依赖于玩家摆脱它。
+my_light_minimum_darkness = 0.01     -- 个人灯打开时的黑暗级别（最初为 0.3）
+                                     -- 所以默认情况下，当你进入静止的机车时，你的个人灯是关闭的
+                                     -- 在机车向前移动之前，周围没有多少光线。
+                                     -- 所以如果你选择“真”，静止的机车周围总会有光。
+my_light_intensity = 2               -- 整体个人光线强度（原为 0.4）
+my_light_size = 100000               -- 整体个人灯光的大小/半径（最初为 25）
+my_light_cone_intensity = 0          -- 个人光锥的强度（原为 0.6）
+
+my_light_spider_vehicle = true       -- 蜘蛛灯不依赖于玩家摆脱它。
+my_light_minimum_darkness = 0.01     -- 个人灯打开时的黑暗级别（最初为 0.3）
+my_light_intensity = 2               -- 整体个人光线强度（原为 0.4）
+my_light_size = 100000               -- 整体个人灯光的大小/半径（最初为 25）
+my_light_cone_intensity = 0          -- 个人光锥的强度（原为 0.6）
+                                   
+my_light_bright = true               --如果你想要一个调光器 gfx 模型的灯，将它设置为 false，对一些用户来说不太亮......
